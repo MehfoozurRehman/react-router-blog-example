569bd8e5da4b8bcba3c26d1f2ea48c0f01c46b54
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-30 18:55:20 +0500

    Update admin.blog._index.tsx

--- app/routes/admin.blog._index.tsx
+++ app/routes/admin.blog._index.tsx
@@ -1,5 +1,6 @@
 import BlogModel from "models/blog";
-import { Link } from "react-router";
+import { useTransition } from "react";
+import { ActionFunctionArgs, Link, useSubmit } from "react-router";
 
 export async function loader() {
   const blogsBase = await BlogModel.find()
@@ -14,6 +15,24 @@ export async function loader() {
   }));
 
   return blogs;
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const body = await request.formData();
+
+  const blogId = body.get("blogId") as string;
+
+  if (!blogId) {
+    return {
+      error: "Blog ID is required",
+    };
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+
+  await BlogModel.findByIdAndDelete(blogId);
+
+  return null;
 }
 
 export default function Blog({
@@ -45,26 +64,58 @@ export default function Blog({
             </Link>
           </div>
           {loaderData.map((blog) => (
-            <div key={blog._id} className="container__content__entry">
-              <div className="container__content__entry__title">
-                {blog.title} <span>({blog.published})</span>
-              </div>
-              <div className="container__content__entry__description">
-                {blog.description}
-              </div>
-              <Link
-                to={`/admin/blog/${blog._id}`}
-                className="container__content__entry__edit"
-              >
-                Edit
-              </Link>
-              <button className="container__content__entry__delete">
-                Delete
-              </button>
-            </div>
+            <BlogEntry key={blog._id} blog={blog} />
           ))}
         </>
       )}
     </>
+  );
+}
+
+function BlogEntry({
+  blog,
+}: {
+  blog: {
+    _id: string;
+    title: string;
+    description: string;
+    published: string;
+  };
+}) {
+  const submit = useSubmit();
+  const [isPending, startTransition] = useTransition();
+
+  const deleteBlog = () => {
+    if (!confirm("Are you sure you want to delete this blog?")) {
+      return;
+    }
+
+    startTransition(async () => {
+      await submit({ blogId: blog._id }, { method: "DELETE" });
+    });
+  };
+
+  return (
+    <div className="container__content__entry">
+      <div className="container__content__entry__title">
+        {blog.title} <span>({blog.published})</span>
+      </div>
+      <div className="container__content__entry__description">
+        {blog.description}
+      </div>
+      <Link
+        to={`/admin/blog/${blog._id}`}
+        className="container__content__entry__edit"
+      >
+        Edit
+      </Link>
+      <button
+        onClick={deleteBlog}
+        disabled={isPending}
+        className="container__content__entry__delete"
+      >
+        {isPending ? "Deleting..." : "Delete"}
+      </button>
+    </div>
   );
 }
