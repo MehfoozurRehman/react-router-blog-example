1b384f3bc3c993763b8ca94321017972ce67fbf1
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-30 18:42:21 +0500

    Update admin.blog.create.tsx

--- app/routes/admin.blog.create.tsx
+++ app/routes/admin.blog.create.tsx
@@ -1,4 +1,41 @@
-import { useFetcher } from "react-router";
+import { ActionFunctionArgs, redirect, useFetcher } from "react-router";
+import BlogModel from "models/blog";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const body = await request.formData();
+
+  const title = body.get("title") as string;
+  const markdownPath = body.get("markdownPath") as string;
+  const description = body.get("description") as string;
+  const published = body.get("published") === "on";
+
+  if (!title) {
+    return {
+      error: "Title is required",
+    };
+  }
+
+  if (!markdownPath) {
+    return {
+      error: "Markdown Path is required",
+    };
+  }
+
+  if (!description) {
+    return {
+      error: "Description is required",
+    };
+  }
+
+  await BlogModel.create({
+    title,
+    markdownPath,
+    description,
+    published,
+  });
+
+  return redirect("/admin/blog");
+}
 
 export default function BlogCreate() {
   const fetcher = useFetcher();
@@ -6,7 +43,7 @@ export default function BlogCreate() {
   return (
     <>
       <div className="container__content__heading">Create Blog</div>
-      <fetcher.Form className="container__content__form">
+      <fetcher.Form method="post" className="container__content__form">
         <input type="text" name="title" placeholder="Title" required />
         <input
           type="text"
@@ -14,12 +51,7 @@ export default function BlogCreate() {
           placeholder="Markdown Path"
           required
         />
-        <input
-          type="text"
-          name="description"
-          placeholder="Description"
-          required
-        />
+        <textarea name="description" placeholder="Description" required />
         <label
           htmlFor="published"
           className="container__content__form__checkbox"
@@ -29,11 +61,17 @@ export default function BlogCreate() {
             name="published"
             id="published"
             placeholder="Published"
-            required
           />
           Published
         </label>
-        <button type="submit">Create</button>
+        <button
+          type="submit"
+          disabled={
+            fetcher.state === "loading" || fetcher.state === "submitting"
+          }
+        >
+          {fetcher.state === "submitting" ? "Creating..." : "Create"}
+        </button>
       </fetcher.Form>
     </>
   );
