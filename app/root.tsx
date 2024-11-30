24249b5e4206cd1bd3a82cf73ef5dedde8f568b1
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-30 17:41:16 +0500

    Update root.tsx

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,14 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import connectDB from "lib/mongoose";
+import {
+  Link,
+  Links,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+} from "react-router";
+
+import "styles/global.scss";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,12 +20,31 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Links />
       </head>
       <body>
-        {children}
+        <div className="container">
+          <div className="container__header">
+            <Link to="/" className="container__header__title">
+              Blogy
+            </Link>
+            <div className="container__header__by">
+              By{" "}
+              <a href="http://github.com/mehfoozurRehman" target="_blank">
+                @mehfoozurRehman
+              </a>
+            </div>
+          </div>
+          <div className="container__content">{children}</div>
+        </div>
         <ScrollRestoration />
         <Scripts />
       </body>
     </html>
   );
+}
+
+export async function loader() {
+  await connectDB();
+
+  return null;
 }
 
 export default function App() {
