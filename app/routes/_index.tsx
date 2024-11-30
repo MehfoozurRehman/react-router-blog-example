21e11d12381f3258e7e9b3cd3b3b73cf90731b7d
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-30 17:41:20 +0500

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,103 +1,37 @@
-import { Suspense, use } from "react";
-import { ActionFunctionArgs, useFetcher } from "react-router";
-
-let data = [
-  {
-    id: 1,
-    post: "Hello World",
-  },
-];
+import BlogModel from "models/blog";
+import { Link } from "react-router";
 
 export async function loader() {
-  const dataPromise = new Promise((resolve) =>
-    setTimeout(() => resolve(data), 1000)
-  );
-
-  return {
-    data: dataPromise,
-  };
-}
-
-export async function action({ request }: ActionFunctionArgs) {
-  const formData = await request.formData();
-
-  if (!formData.get("post")) {
-    return {
-      error: "Post is required",
-    };
-  }
-
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-
-  data.push({
-    id: data.length + 1,
-    post: formData.get("post") as string,
-  });
-
-  return {
-    message: "Post created",
-  };
+  return await BlogModel.find({
+    published: true,
+  }).select("title description _id");
 }
 
 export default function Blog({
   loaderData,
 }: {
   loaderData: {
-    data: Promise<
-      {
-        id: number;
-        post: string;
-      }[]
-    >;
-  };
+    _id: string;
+    title: string;
+    description: string;
+  }[];
 }) {
-  const fetcher = useFetcher();
-
   return (
-    <div>
-      <h1>Blog</h1>
-      <Suspense
-        fallback={
-          <div>
-            <h2>Loading...</h2>
-          </div>
-        }
-      >
-        <List
-          dataPromise={loaderData.data}
-          isLoading={fetcher.state === "loading"}
-        />
-      </Suspense>
-      <fetcher.Form method="post" key={fetcher.state}>
-        <input type="text" name="post" />
-        <button disabled={fetcher.state === "submitting"}>
-          {fetcher.state === "submitting" ? "Submitting..." : "Submit"}
-        </button>
-        {fetcher?.data?.message && (
-          <p style={{ color: "green" }}>{fetcher.data.message}</p>
-        )}
-        {fetcher?.data?.error && (
-          <p style={{ color: "red" }}>{fetcher.data.error}</p>
-        )}
-      </fetcher.Form>
-    </div>
-  );
-}
-
-function List({
-  dataPromise,
-  isLoading,
-}: {
-  dataPromise: Promise<{ id: number; post: string }[]>;
-  isLoading: boolean;
-}) {
-  const list = use(dataPromise);
-
-  return (
-    <ul style={{ opacity: isLoading ? 0.5 : 1 }}>
-      {list?.map((post) => (
-        <li key={post.id}>{post.post}</li>
-      ))}
-    </ul>
+    <>
+      {loaderData.length === 0 ? (
+        <div className="container__content__no__data">No blogs found</div>
+      ) : (
+        loaderData.map((blog) => (
+          <Link
+            to={`/${blog._id}`}
+            key={blog._id}
+            className="container__content__entry"
+          >
+            <h2>{blog.title}</h2>
+            <p>{blog.description}</p>
+          </Link>
+        ))
+      )}
+    </>
   );
 }
