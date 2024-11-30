25ce5fca0afd617abc614c4ce759232739e7d7ce
Mehfooz-ur-Rehman <68416499+MehfoozurRehman@users.noreply.github.com>
2024-11-30 18:42:15 +0500

    Update admin.blog.$blogId.tsx

--- app/routes/admin.blog.$blogId.tsx
+++ app/routes/admin.blog.$blogId.tsx
@@ -1,24 +1,99 @@
-import { useFetcher } from "react-router";
+import {
+  ActionFunctionArgs,
+  LoaderFunctionArgs,
+  redirect,
+  useFetcher,
+} from "react-router";
+import BlogModel from "models/blog";
 
-export default function BlogEdit() {
+export async function loader({ params }: LoaderFunctionArgs) {
+  const blogId = params.blogId;
+
+  const blog = await BlogModel.findById(blogId)
+    .select("title description markdownPath published")
+    .lean();
+
+  return blog;
+}
+
+export async function action({ params, request }: ActionFunctionArgs) {
+  try {
+    const blogId = params.blogId;
+    const body = await request.formData();
+
+    const title = body.get("title") as string;
+    const markdownPath = body.get("markdownPath") as string;
+    const description = body.get("description") as string;
+    const published = body.get("published") === "on";
+
+    if (!title) {
+      return {
+        error: "Title is required",
+      };
+    }
+
+    if (!markdownPath) {
+      return {
+        error: "Markdown Path is required",
+      };
+    }
+
+    if (!description) {
+      return {
+        error: "Description is required",
+      };
+    }
+
+    await BlogModel.findByIdAndUpdate(blogId, {
+      title,
+      markdownPath,
+      description,
+      published,
+    });
+
+    return redirect("/admin/blog");
+  } catch (e) {
+    return {
+      error: (e as Error).message,
+    };
+  }
+}
+
+export default function BlogEdit({
+  loaderData,
+}: {
+  loaderData: {
+    title: string;
+    description: string;
+    markdownPath: string;
+    published: boolean;
+  };
+}) {
   const fetcher = useFetcher();
 
   return (
     <>
       <div className="container__content__heading">Edit Blog</div>
-      <fetcher.Form className="container__content__form">
-        <input type="text" name="title" placeholder="Title" required />
+      <fetcher.Form method="post" className="container__content__form">
+        <input
+          type="text"
+          name="title"
+          placeholder="Title"
+          required
+          defaultValue={loaderData.title}
+        />
         <input
           type="text"
           name="markdownPath"
           placeholder="Markdown Path"
           required
+          defaultValue={loaderData.markdownPath}
         />
-        <input
-          type="text"
+        <textarea
           name="description"
           placeholder="Description"
           required
+          defaultValue={loaderData.description}
         />
         <label
           htmlFor="published"
@@ -29,11 +104,11 @@ export default function BlogEdit() {
             name="published"
             id="published"
             placeholder="Published"
-            required
+            defaultChecked={loaderData.published}
           />
           Published
         </label>
-        <button type="submit">Create</button>
+        <button type="submit">Save</button>
       </fetcher.Form>
     </>
   );
